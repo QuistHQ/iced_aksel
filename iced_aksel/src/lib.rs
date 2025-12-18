2f7d28bbbfd98b02ef45f16f10e258231d76688f
MellquistD <dennis.mellquist@gmail.com>
2025-12-18 13:49:11 +0100

    Removed unnecessary background color code

--- iced_aksel/src/lib.rs
+++ iced_aksel/src/lib.rs
@@ -1010,15 +1010,6 @@ where
         let bounds = layout.bounds();
         let plot_bounds = self.get_plot_layout(layout).bounds();
 
-        // Fill in background
-        renderer.fill_quad(
-            Quad {
-                bounds,
-                ..Default::default()
-            },
-            Color::TRANSPARENT,
-        );
-
         // 1. Retrieve the Memory from the Tree directly
         let memory = tree.state.downcast_ref::<Memory<AxisId>>();
 
