fe50025b82b2e5233d274b4a7429a065280622f3
Mads Jensen <madsaj10@outlook.com>
2026-03-19 13:26:38 +0100

    initial commit

--- iced_aksel/src/interaction/query.rs
+++ iced_aksel/src/interaction/query.rs
@@ -1,4 +1,8 @@
+use crate::IntoHandler;
+
+use super::{Id, ResolvedInteraction};
 use iced_core::{Point, Rectangle};
+use std::hash::Hash;
 
 /// Represents a spatial query in screen-space to test against interactions.
 #[derive(Debug, Clone, Copy)]
@@ -26,5 +30,69 @@ impl InteractionQuery {
             },
             Self::Bounds(rect) => *rect,
         }
+    }
+}
+
+type QueryHandler<Message, R> = crate::event::Handler<Message, (R,)>;
+type QueryFilter<Message, Tag> = Box<dyn Fn(Id<Tag>, ResolvedInteraction<Message, Tag>) -> bool>;
+
+pub enum QueryMode<Message, Tag: Hash + Eq + Clone> {
+    Closest(QueryHandler<Message, Id<Tag>>),
+    All(QueryHandler<Message, Vec<Id<Tag>>>),
+    Any(QueryHandler<Message, Id<Tag>>),
+}
+
+pub struct QueryBuilder {
+    area: Rectangle,
+}
+
+impl QueryBuilder {
+    pub fn mode<Message: Clone, Tag: Hash + Eq + Clone>(
+        self,
+        mode: QueryMode<Message, Tag>,
+    ) -> Query<Message, Tag> {
+        let Self { area } = self;
+        Query {
+            filter: None,
+            prioritized: false,
+            mode,
+            area,
+        }
+    }
+
+    pub fn closest<Message: Clone, Tag: Hash + Eq + Clone, F>(self, f: F) -> Query<Message, Tag>
+    where
+        F: IntoHandler<Message, (Id<Tag>,)>,
+    {
+        let Self { area } = self;
+        Query {
+            filter: None,
+            mode: QueryMode::Closest(f.into_handler()),
+            prioritized: false,
+            area,
+        }
+    }
+}
+
+pub struct Query<Message: Clone, Tag: Hash + Eq + Clone> {
+    filter: Option<QueryFilter<Message, Tag>>,
+    mode: QueryMode<Message, Tag>,
+    prioritized: bool,
+    area: Rectangle,
+}
+
+impl<Message: Clone, Tag: Hash + Eq + Clone> Query<Message, Tag> {
+    pub fn point(point: Point, tolerance_px: f32) -> QueryBuilder {
+        let area = Rectangle {
+            x: point.x - tolerance_px,
+            y: point.y - tolerance_px,
+            width: tolerance_px * 2.0,
+            height: tolerance_px * 2.0,
+        };
+        QueryBuilder { area }
+    }
+
+    pub const fn rect(rect: Rectangle) -> QueryBuilder {
+        QueryBuilder { area: rect }
     }
 }
